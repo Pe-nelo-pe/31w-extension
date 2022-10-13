34e0603121cecb9532d1bb51294744cf1eef26e0
Pénélope Ducharme <88218376+Pe-nelo-pe@users.noreply.github.com>
2022-10-13 12:02:36 -0400

    c9 - modification plugin du fichier php

--- nelo_carrousel.php
+++ nelo_carrousel.php
@@ -10,17 +10,33 @@ Plugin Name: nelo_carrousel
 Version 1.0.0
 */
 
+// filemtime();  // retourne en milliseconde le temps de la dernière sauvegarde
+// plugin_dir_path(); // retourne le chemin du répertoire du plugin
+// __FILE__ ;// le fichier en train de s'exécuter
+// wp_enqueue_style(); // Intègre le link:css dans la page
+// wp_enqueue_script(); // intègre le script dans la page
+// wp_enqueue_scripts();
+
+function neloc_enqueue(){
+    $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
+    $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
+
+    wp_enqueue_style("neloc_style_carrousel",
+                     plugin_dir_url(__FILE__) . "style.css",
+                     array(),
+                     $version_css,
+                     false);
+    wp_enqueue_script("neloc_js_carrousel",
+                     plugin_dir_url(__FILE__) . "js/carrousel.js",
+                     array(),
+                     $version_js,
+                     false);
+}
+
+add_action("wp_enqueue_scripts", "neloc_enqueue");
+
 function genere_boite(){
-    $contenu = "
-    <style>
-        .carrousel{
-            border: 2px solid black;
-            background-color: green;
-            heigth: 200px;
-            padding: 25px;
-        }
-    </style>
-    <div class = 'carrousel'>Carrousel</div>";
+    $contenu = "<div class = 'carrousel'>Carrousel</div>";
     return $contenu;
 }
 
